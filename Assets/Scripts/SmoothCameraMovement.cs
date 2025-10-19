241ae73610ac7a028e09d2cbc7ab9a907bd1b9e5
MFurkan <mehmetfurkanturkoglu@gmail.com>
2025-10-19 16:52:19 +0300

    Camera Yırtılmaları Düzeltildi

--- Assets/Scripts/SmoothCameraMovement.cs
+++ Assets/Scripts/SmoothCameraMovement.cs
@@ -6,11 +6,25 @@ public class SmoothCameraMovement : MonoBehaviour
    [SerializeField]private Vector3 offset;
    public Transform target;
   private Vector3 vel = Vector3.zero;
-   private void Update()
-   {
-     Vector3 targetPosition = target.position + offset;
-    targetPosition.z=transform.position.z;
-     transform.position = Vector3.SmoothDamp(transform.position, targetPosition,ref
-     vel,damping);
-   }
+  private void Update()
+  {
+    Vector3 targetPosition = target.position + offset;
+    targetPosition.z = transform.position.z;
+    transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref
+    vel, damping);
+  }
+   
+   // CameraFollow.cs
+void LateUpdate()
+{
+    Vector3 pos = target.position;
+    pos.z = transform.position.z;
+
+    // 32, sprite başına piksel sayısı (Pixels Per Unit)
+    pos.x = Mathf.Round(pos.x * 32f) / 32f;
+    pos.y = Mathf.Round(pos.y * 32f) / 32f;
+
+    transform.position = pos;
+}
+
 } 
